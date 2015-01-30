39f132aefe7b60a63c2c97638a5b9f7dc7cd74aa
Jason Monsef <jasonmonsef@gmail.com>
2015-01-30 00:16:42 -0800

    menu refinements
    menu refinements

--- wp-content/themes/drbaker/header.php
+++ wp-content/themes/drbaker/header.php
@@ -62,11 +62,7 @@
 					<li><a href="<?php bloginfo('url') ?>">Contact<br>Us</a></li>
 				</ul>
 -->
-				<ul>
-      <?php if (is_page()) { $highlight = "page_item"; } else {$highlight = "page_item current_page_item"; } ?>
-      <li class="<?php echo $highlight; ?>"><a href="<?php bloginfo('url'); ?>">Home</a></li>
-      <?php wp_list_pages('sort_column=menu_order&depth=1&title_li='); ?>
-    </ul>
+				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
 			</div>
 		</div>
 		
